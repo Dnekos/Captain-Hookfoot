5bd232187a8f03ead6a04901ed8afc73f4867b23
eli.moore <eli.moore@mymail.champlain.edu>
2021-04-03 17:20:16 -0400

    Create DatabaseNode Script and add functions
    * Might be temporary; might not be.
    * InitDatabase function sets everything up
    * FetchTextByID function gets the data specified by 'term' from the row
      specified by 'id'
    * I would like to comment my code more; but I ran out of time for today so I will do it soon.

---
+++ Assets/Scripts/DatabaseNode.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using System.Data;
+using Mono.Data.Sqlite;
+using System.IO;
+
+public class DatabaseNode : MonoBehaviour
+{
+
+    string connection;
+    IDbConnection dbcon;
+    IDataReader reader;
+    
+    string dbName = "Project3.db";
+
+    void OpenDatabase()
+    {
+
+        // Open connection
+        dbcon.Open();
+    }
+
+    void InitDatabase()
+    {
+        //Setup file with url
+        connection = "URI=file:" + Application.dataPath + "/" + dbName;
+        dbcon = new SqliteConnection(connection);
+    }
+
+    string FetchTextByID(int id, string term)
+    {
+        // READING DATA
+        // Read and print all values in table
+        dbcon.Open();
+        IDbCommand cmnd_read = dbcon.CreateCommand();
+        string query = "SELECT " + term + " FROM NodeDialogue WHERE ID=" + id;
+        cmnd_read.CommandText = query;
+        reader = cmnd_read.ExecuteReader();
+        string output = reader[0].ToString();
+        dbcon.Close();
+        return output;
+    }
+
+    void Start()
+    {
+        InitDatabase();
+        Debug.Log(FetchTextByID(1, "LookDialogue"));
+        Debug.Log(FetchTextByID(2, "LookDialogue"));
+    }
+}
+
