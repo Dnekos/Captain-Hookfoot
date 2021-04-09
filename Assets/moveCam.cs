72b2596487ad2618441e815daa598fb47f7e6beb
Demetri <dnekos2462@gmail.com>
2021-04-09 17:22:36 -0400

    Camera moves in scenes

--- Assets/moveCam.cs
+++ Assets/moveCam.cs
@@ -10,19 +10,22 @@ public class moveCam : MonoBehaviour
     float speed = 1;
     [SerializeField]
     SpriteRenderer border;
+    Camera cam;
     private void Start()
     {
-        Camera cam = GetComponent<Camera>();
+        cam = GetComponent<Camera>();
         screenHeight = cam.scaledPixelHeight;
         screenWidth = cam.scaledPixelWidth;
     }
     private void Update()
     {
+        float diff = speed * Time.deltaTime, spriteradius = border.sprite.bounds.size.x * 0.5f;
         Vector2 mouse = Mouse.current.position.ReadValue();
-        if (mouse.x > screenWidth - 30 && mouse.x + speed * Time.deltaTime < border.sprite.border.w)
-            transform.position += Vector3.right * speed * Time.deltaTime;
-        else if (mouse.x < 30 && mouse.x + speed * Time.deltaTime > border.sprite.border.x)
-            transform.position -= Vector3.right * speed * Time.deltaTime;
+        Debug.Log(spriteradius + " " + spriteradius / 50f + " " + spriteradius * 0.05f);
+        if (mouse.x > screenWidth - 30 && cam.ScreenToWorldPoint(new Vector2(screenWidth, 0)).x + diff + spriteradius * 0.06 < border.transform.position.x + spriteradius)
+            transform.position += Vector3.right * diff;
+        else if (mouse.x < 30 && cam.ScreenToWorldPoint(new Vector2(0, 0)).x - diff - spriteradius * 0.06 > border.transform.position.x - spriteradius)
+            transform.position += Vector3.left * diff;
 
     }
 }
